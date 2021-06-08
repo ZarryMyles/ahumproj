d86893e26844aa79d6d678fe6852462600f1dc11
ZarryMyles <mahanth2709@gmail.com>
2021-06-08 19:51:38 +0530

    Added Form Validations

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,9 +1,119 @@
-import React, { useEffect } from "react";
-import Navbar from "./Navbar";
+import React, { useEffect, useState } from "react";
 import $ from "jquery";
-import TopLogoBar from "./TopLogoBar";
 
 function Contact() {
+  //Form Handlers
+
+  const [borderColor, setBorderColor] = useState("white");
+  const [formDetails, setFormDetails] = useState({
+    name: "",
+    phno: "",
+    mail: "",
+    msg: "",
+    error: {
+      name: "Enter a vaild Name",
+      mail: "Enter a vaild Email Address",
+      phno: "Enter a vaild Phone Number",
+      msg: "Enter a vaild Message",
+    },
+  });
+  const [newsDetails, setNewsDetails] = useState({
+    nMail: "",
+    nError: "Enter a valid E-mail address",
+  });
+
+  const validateEmail = RegExp(
+    /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+  );
+  const validatePhone = RegExp(/^\d{10}$/i);
+
+  let changeHandler = (event) => {
+    let nam = event.target.name;
+    let val = event.target.value;
+
+    switch (nam) {
+      case "name":
+        setFormDetails((prevState) => ({
+          ...prevState,
+          error: {
+            ...prevState.error,
+            name: val.length < 2 ? "Enter a vaild Name" : "",
+          },
+        }));
+        break;
+
+      case "mail":
+        setFormDetails((prevState) => ({
+          ...prevState,
+          error: {
+            ...prevState.error,
+            mail: validateEmail.test(val) ? "" : "Enter a valid email address",
+          },
+        }));
+        break;
+
+      case "phno":
+        setFormDetails((prevState) => ({
+          ...prevState,
+          error: {
+            ...prevState.error,
+            phno: validatePhone.test(val) ? "" : "Enter a valid Phone Number",
+          },
+        }));
+        break;
+
+      case "msg":
+        setFormDetails((prevState) => ({
+          ...prevState,
+          error: {
+            ...prevState.error,
+            msg: val.length < 5 ? "Enter a valid message!" : "",
+          },
+        }));
+        break;
+      default:
+        break;
+    }
+    setFormDetails((prevState) => ({ ...prevState, [nam]: val }));
+  };
+
+  let handleSubmit = async (event) => {
+    event.preventDefault();
+    const validateForm = (errors) => {
+      let valid = true;
+      Object.keys(errors).forEach((key) => {
+        if (errors[key].length !== 0 && valid === true) {
+          alert(errors[key]);
+          valid = false;
+        }
+      });
+      return valid;
+    };
+    if (validateForm(formDetails.error)) {
+      console.log({
+        Name: formDetails.name,
+        Mail: formDetails.mail,
+        PhNo: formDetails.phno,
+        Message: formDetails.msg,
+        Date: new Date().toLocaleString(),
+      });
+
+      setBorderColor("green"); //Changes field border color to green
+
+      // Reset checkboxes and fields
+      document.getElementById("cForm").reset();
+      document.getElementById("name").placeholder = "Your Name";
+      document.getElementById("mail").placeholder = "Email";
+      document.getElementById("phno").placeholder = "Phone";
+      document.getElementById("msg").placeholder = "Your Message";
+
+      console.info("Valid Form");
+    } else {
+      setBorderColor("red");
+      console.error("Invalid Form");
+    }
+  };
+
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
@@ -15,7 +125,7 @@ function Contact() {
   };
 
   const inputCSS =
-    "focus:placeholder-gray-200 focus:outline-none focus:shadow-xl hover:shadow-md placeholder-gray-300 mb-3 py-2 px-2 w-full border-solid border border-black";
+    "focus:placeholder-gray-500 focus:outline-none focus:shadow-xl hover:shadow-md placeholder-gray-400 mb-3 py-2 px-2 w-full border-solid border";
 
   return (
     <div class="wrapper">
@@ -31,44 +141,66 @@ function Contact() {
                   <span className="font-normal mb-3">
                     Drop us a message, we'll call you back!
                   </span>
-                  <input
-                    className={inputCSS}
-                    type="text"
-                    name="name"
-                    placeholder="Your Name"
-                  />
-                  <div className="grid grid-cols-5 gap-4">
-                    <div className="col-span-2">
-                      <input
-                        className={inputCSS}
-                        type="text"
-                        name="name"
-                        placeholder="Phone"
-                      />
+                  <form id="cForm">
+                    <input
+                      className={inputCSS}
+                      type="text"
+                      id="name"
+                      style={{ borderColor: borderColor }}
+                      name="name"
+                      placeholder="Your Name"
+                      onChange={(event) => {
+                        changeHandler(event);
+                      }}
+                    />
+                    <div className="grid grid-cols-5 gap-4">
+                      <div className="col-span-2">
+                        <input
+                          className={inputCSS}
+                          type="text"
+                          name="phno"
+                          id="phno"
+                          placeholder="Phone"
+                          onChange={(event) => {
+                            changeHandler(event);
+                          }}
+                        />
+                      </div>
+                      <div className="col-span-3">
+                        <input
+                          className={inputCSS}
+                          type="text"
+                          name="mail"
+                          id="mail"
+                          placeholder="Email"
+                          onChange={(event) => {
+                            changeHandler(event);
+                          }}
+                        />
+                      </div>
                     </div>
-                    <div className="col-span-3">
-                      <input
-                        className={inputCSS}
-                        type="text"
-                        name="name"
-                        placeholder="Email"
-                      />
-                    </div>
-                  </div>
-                  <textarea
-                    className={inputCSS}
-                    type="text"
-                    name="name"
-                    placeholder="Your Message"
-                  />
+                    <textarea
+                      className={inputCSS}
+                      type="text"
+                      name="msg"
+                      id="msg"
+                      placeholder="Your Message"
+                      onChange={(event) => {
+                        changeHandler(event);
+                      }}
+                    />
 
-                  <button
-                    className="my-1 py-1 md:w-1/5 bg-ahum-brown transition duration-500 ease-in-out transform md:hover:scale-105 hover:shadow-xl text-white text-lg"
-                    type="submit"
-                    value="Send Message"
-                  >
-                    Submit
-                  </button>
+                    <button
+                      className="my-1 py-1 md:w-1/5 bg-ahum-brown transition duration-500 ease-in-out transform md:hover:scale-105 hover:shadow-xl text-white text-lg"
+                      type="submit"
+                      value="Send Message"
+                      onClick={(event) => {
+                        handleSubmit(event);
+                      }}
+                    >
+                      Submit
+                    </button>
+                  </form>
                 </div>
               </div>
               <div className="xs:mt-4 md:mt-0 flex justify-center col-span-1 items-center ml-10 pb-4">
@@ -100,14 +232,15 @@ function Contact() {
                   </div>
                   <div className="lg:col-start-4 col-end-6 w-5/6 md:w-4/6 flex flex-col">
                     <input
-                      className="newsField text-white text-sm bg-ahum-brown border-b-0"
+                      id="newsLetter"
+                      className="newsField text-white text-base bg-ahum-brown border-b-0 focus:bg-transparent focus:placeholder-transparent"
                       placeholder="Email Address"
                     ></input>
                     <div className="border-t border-white border"></div>
                     <button
-                      className="newsBtn  px-2 mt-2 bg-ahum-brown-collage transition duration-500 ease-in-out transform md:hover:scale-105 hover:shadow-xl text-grey-600 text-lg"
+                      className="newsBtn  px-2 mt-2 bg-ahum-brown-collage transition duration-500 ease-in-out transform md:hover:scale-105 hover:shadow-xl text-grey-600 text-lg focus:border-none"
                       type="submit"
-                      name="name"
+                      name="emailSubmit"
                     >
                       Submit
                     </button>

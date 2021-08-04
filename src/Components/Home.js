9b0397aa2e39d1bd978e06f75de7bc87e469eade
ZarryMyles <mahanth2709@gmail.com>
2021-08-04 18:56:03 +0530

    Final changes

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import Navbar from "./Navbar";
 import $ from "jquery";
+import homeVid from "../Images/ahumhomepage.mp4";
 
 function Home() {
   useEffect(() => {
@@ -10,7 +11,19 @@ function Home() {
   return (
     <div class="wrapper">
       <div id="content">
-        <div class="container-fluid justify-content-center"></div>
+        <div class="justify-content-center flex items-center justify-center h-full">
+          <video
+            className="mb-40"
+            width={window.innerWidth > 767 ? "70%" : "100%"}
+            loop
+            controlsList="nodownload"
+            preload="auto"
+            controls
+          >
+            <source src={homeVid} type="video/mp4" />
+            There was a problem playing the video on your browser...
+          </video>
+        </div>
       </div>
     </div>
   );

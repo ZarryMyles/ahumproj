0539c9fbd3f9c2d7e34c9c5d87f8c203f76167e4
richinrix <richinvishal@gmail.com>
2021-08-04 20:51:19 +0530

    upcoming page update

--- src/Components/UpcomingShows.js
+++ src/Components/UpcomingShows.js
@@ -77,7 +77,7 @@ function UpcomingShows() {
     // when no upcoming shows present
     if (!loading)
       return (
-        <div className="font-montserrat w-full h-96  flex flex-col items-center justify-center">
+        <div className="font-montserrat w-full h-96  flex flex-col items-center justify-center ">
           <h1 className=" text-center   text-ahum-maroon font-semibold md:text-4xl text-2xl mb-10">
             No Upcoming Shows
           </h1>

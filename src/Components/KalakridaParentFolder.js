a0f1a2cf0aeea4ae969f6712f6e19dede8f676ec
richinrix <richinvishal@gmail.com>
2021-08-17 15:48:27 +0530

    kalakrida

--- src/Components/KalakridaParentFolder.js
+++ src/Components/KalakridaParentFolder.js
@@ -13,29 +13,32 @@ export default function KalakridaParentFolder(props) {
       <div className="flex text-ahum-brown  justify-end w-full pr-5">
         <a href="/archives/">&#129040; Back</a>
       </div>
-      <div className="flex md:flex-row flex-col justify-around my-5 text-3xl text-center  text-ahum-maroon ">
-        <a href={`/kalakrida/gallery/` + year}>
-          <div className="flex flex-col  ring-ahum-maroon p-20 rounded-lg hover:bg-ahum-brown-collage duration-300">
-            <img
-              src={FolderIcon}
-              alt="folder"
-              className="img-fluid"
-              width="150px"
-            />
-            <div className="mx-2">Gallery</div>
-          </div>
+      <div className="flex md:flex-row flex-col justify-around  text-3xl text-center  text-ahum-maroon  ">
+        <a
+          className=" hover:bg-ahum-brown-collage p-10 rounded-xl"
+          href={`/kalakrida/gallery/` + year}
+        >
+          <div
+            className="  div-bg-pic flex flex-col ring-2 overflow-hidden  shadow-lg    items-center  ring-ahum-maroon rounded-lg hover:bg-ahum-brown-collage duration-300"
+            style={{
+              backgroundImage: `url('https://res.cloudinary.com/dtfecnt6o/image/upload/v1629194387/kalakrida-gallery/Untitled_2_t6znvr.png')`,
+              width: "47vh",
+              height: "32.5vh",
+            }}
+          ></div>
+          <div className="m-3">Gallery</div>
         </a>
 
         <a href={`/kalakrida/report/` + year}>
-          <div className="flex flex-col ring-ahum-maroon p-20  rounded-lg hover:bg-ahum-brown-collage duration-300">
-            <img
+          <div className="flex flex-col w-1/2 ring-2 ring-ahum-maroon p-20  rounded-lg hover:bg-ahum-brown-collage duration-300">
+            {/* <img
               src={ReportIcon}
               width="150px"
               alt="folder"
               className="img-fluid"
-            />
-            <div className="mx-2 mt-1">Report</div>
+            /> */}
           </div>
+          <div className="mx-2 mt-1">Report</div>
         </a>
       </div>
     </div>
